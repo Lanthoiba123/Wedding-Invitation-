6e92de603e5b9a8dfe7084540caf8f3576404b98
Lan_wifi <lanthoiba_k20@mtu.ac.in>
2024-05-27 08:14:58 +0530

    add new

--- src/image.jsx
+++ src/image.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useDisclosure } from '@mantine/hooks';
+import { useDisclosure } from "@mantine/hooks";
 import { Modal } from "@mantine/core";
 import { Link } from "react-router-dom";
 import "./App.css";
 
-
 function App() {
   const [showFlower, setShowFlower] = useState(false);
   const [opened, { open, close }] = useDisclosure(false);
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -20,35 +18,60 @@ function App() {
 
   return (
     <div>
-        <Modal opened={opened} onClose={close} title="Location">
-          <div className="flex flex-col">
-
-          <a className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="https://maps.app.goo.gl/wRrcdM1cJJUDS5eG7" target="_blank">Bor Location</a>
-          <a className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="https://maps.app.goo.gl/wRrcdM1cJJUDS5eG7" target="_blank">Keina Location</a>
-          </div>
+      <Modal opened={opened} onClose={close} title="Location">
+        <div className="flex flex-col">
+          <a
+            className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            href="https://maps.app.goo.gl/wRrcdM1cJJUDS5eG7"
+            target="_blank"
+          >
+            Bor Location
+          </a>
+          <a
+            className="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            href="https://maps.app.goo.gl/wRrcdM1cJJUDS5eG7"
+            target="_blank"
+          >
+            Keina Location
+          </a>
+        </div>
       </Modal>
       <div
         className="relative h-screen bg-cover flex justify-center"
-        style={{ backgroundImage: "url('img.jpeg')" }}
+        style={{ backgroundImage: "url('together_pic.jpg')" }}
       >
-        <div
+        {/* <div
           className={`absolute inset-0 bg-cover ${
             showFlower ? "squeeze-in-out" : ""
           }`}
           style={{ backgroundImage: "url('flower.png')" }}
-        />
-        <Link to='/video' className=" absolute text-3xl text-center text-white rounded-lg px-2  bg-gray-600 mt-[480px] ring-4 ring-teal-400">
+        /> */}
+        <Link
+          to="/video"
+          className="gradiant absolute text-xl text-center text-white rounded-lg px-2  bg-gray-600 mt-[550px] ring-4 ring-teal-400"
+        >
           Click here for sweet video
         </Link>
-        <div className=" absolute text-2xl text-center text-white rounded-lg px-2  bg-gray-600 mt-[530px]" onClick={open}>
+        <div
+          className="absolute text-2xl text-center text-white rounded-lg px-2  bg-gray-600 mt-[600px]"
+          onClick={open}
+        >
           Location
         </div>
-        <div className=" top-6 text-white  absolute font-satisfy">
-          <h1 className="text-7xl font-semibold">Heijingpot</h1>
-          <p className="text-3xl">14 June 2024</p>
+        <div className=" top-16  text-orange-600  absolute font-satisfy">
+          <h1 className="text-xl font-thin">WEDDING INVITATION</h1>
+          {/* <p className="text-xl">14 June 2024</p> */}
           {/* <p className="text-3xl">Venue</p> */}
           {/* <p className="text-3xl">Patsoi Part-1 Awang Leikai</p> */}
         </div>
+        <img className="size-40 mt-8" src="design.png" alt="" />
+      </div>
+      <div className="absolute top-[410px] text-balance w-full text-teal-400 flex justify-center items-center flex-col font-serif ">
+        <h1>Kangabam Aneet Mangang</h1>
+        <h1>&</h1>
+        <h1>Leichombam Mamata Devi</h1>
+        <h1>on</h1>
+        <h1 className="text-orange-600">FRIDAY | 14th JUNE 2024</h1>
       </div>
     </div>
   );
--- src/video.jsx
+++ src/video.jsx
@@ -4,16 +4,11 @@ const Video = () => {
 
   useEffect(() => {
     const video = videoRef.current;
-    
-
-    // const playVideo = () => {
-      if (video) {
-        video.muted=false
-        video.play().catch((error) => {
-          console.error("Error attempting to play the video:", error);
-        });
-      // }
-    // playVideo()
+    if (video) {
+      video.muted = true; // Mute the video to ensure autoplay works
+      video.play().catch((error) => {
+        console.error("Error attempting to play the video:", error);
+      });
     }
   }, []);
   return (
@@ -23,11 +18,9 @@ const Video = () => {
         memories
       </h1>
       <div className="aspect-video h-[500px] w-full flex justify-center mt-6">
-        <video ref={videoRef} controls autoPlay >
+        <video ref={videoRef} controls autoPlay muted>
           <source src="checkVideo.mp4" type="video/mp4" />
         </video>
-        {/* <iframe src="checkVideo.mp4" allow="autoplay"></iframe> */}
-
       </div>
       <h1>Thank You For Watching Our Sweet Memories Video</h1>
     </div>
